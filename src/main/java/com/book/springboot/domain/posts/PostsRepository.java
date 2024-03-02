4eaedfdaf39669a37ae0c1bb23f257c85bee98a5
cje614 <choije614@gmail.com>
2024-03-02 21:21:33 +0900

    Mustache 화면 구성

--- src/main/java/com/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/book/springboot/domain/posts/PostsRepository.java
@@ -1,8 +1,15 @@
 package com.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    // SpringDataJpa에서 제공하는 기본 메소드를 사용해도 되지만, 제공하지 않는 메소드는 아래처럼 @Query 를 사용
+    @Query("SELECT p FROM Posts  p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
 
 /*
--- src/main/java/com/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.book.springboot.service.posts;
 
 import com.book.springboot.domain.posts.Posts;
 import com.book.springboot.domain.posts.PostsRepository;
+import com.book.springboot.web.dto.PostsListResponseDto;
 import com.book.springboot.web.dto.PostsResponseDto;
 import com.book.springboot.web.dto.PostsSaveRequestDto;
 import com.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor // final이 선언된 모든 필드를 인자값으로 하는 생성자를 대신 생성해줌
 @Service
@@ -36,5 +40,20 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true) // readOnly 옵션을 주면 트랜잭션 범위는 유지하되, 조회 기능만 남겨두어 조회속도가 개선되기 때문에 등록,수정,삭제 기능이 전혀 없는 서비스 메소드에서 사용하는 것을 추천
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) // .map(posts -> new PostsListResponseDto(posts))와 같다. postsRepository 결과로 넘어온 Posts의 Stream을 map을 통해 PostsListResponseDto 변환 -> List로 반환하는 메소드
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+         Posts posts = postsRepository.findById(id)
+                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+
+         postsRepository.delete(posts);
     }
 }
